5afc697a00afb041c6d953abb6a16fb3050f36de
hemalatha <avhemavinoth@gmail.com>
2016-08-30 12:01:15 +0530

    Update and rename ana.java to multiply.java

---
+++ multiply.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.Scanner.*;
+public class multiply {
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+			int b=Integer.parseInt(args[1]);
+			int c;
+	c=a*b;
+		String d=String.valueOf(c);
+			System.out.println("The multiplication of two string integer is:"+d);
+	}
+
+}
